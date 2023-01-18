421190ff1ebf465d19a7c47c54a21b16fd16603f
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-01-19 02:23:54 +0600

    Add files via upload

---
+++ ArrayList/array_list.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class array_list {
+    public static void main(String[] args) {
+        ArrayList<Integer> li = new ArrayList<>();
+        li.add(1);
+        li.add(9);
+        li.add(3);
+        li.add(5);
+        System.out.println(li);
+
+        System.out.println(li.get(0));// find spacific position
+        li.add(0, 0);// insert between
+        System.out.println(li);
+        li.set(4, 4);// replace the element
+        System.out.println(li);
+
+        // remove
+        li.remove(0);
+        System.out.println(li);
+
+        System.out.println(li.size());
+
+        for (int i = 0; i < li.size(); i++) {
+            System.out.print(li.get(i) + " ");
+
+        }
+        System.out.println();
+
+        // sort
+        Collections.sort(li);
+        System.out.println(li);
+
+    }
+
+}
