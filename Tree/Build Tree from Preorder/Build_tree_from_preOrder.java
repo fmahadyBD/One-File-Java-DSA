9a88a207d1dc4a46e46c3a72c40e324a0655a296
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-01-19 12:56:37 +0600

    Add files via upload

---
+++ Tree/Build Tree from Preorder/Build_tree_from_preOrder.java
@@ -0,0 +1,41 @@
+public class Build_tree_from_preOrder {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+    static class binararyTree {
+
+        static int indx = -1;
+
+        public static Node buildTree(int nodes[]) {
+
+            indx++;
+            if (nodes[indx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[indx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        binararyTree obj = new binararyTree();
+        Node root = obj.buildTree(nodes);
+        System.out.println(root.data);
+
+    }
+}
