300c07c1f4adc7934cb31cf01249c4c123527f5a
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-01-19 18:28:53 +0600

    Add files via upload

---
+++ Tree/Inorder Tree/InOrderTravarsal.java
@@ -0,0 +1,50 @@
+public class InOrderTravarsal {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+
+    static class binararyTree {
+
+        static int index = -1;
+
+        public static Node buildTree(int nodes[]) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[index]);
+
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+
+        }
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        binararyTree obj = new binararyTree();
+        Node root = obj.buildTree(nodes);
+        inorder(root);
+
+    }
+
+}
